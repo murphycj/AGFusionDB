7f7ccd8919650ed59f67e233c0c5f7ecac2378bc
Charles Murphy <murphy.charlesj@gmail.com>
2017-07-19 11:41:26 -0400

    scripts

---
+++ build_db.sh
@@ -0,0 +1,37 @@
+#curl ftp://ftp.ebi.ac.uk/pub/builds/Pfam/current_release/build_files//pfamA.txt.gz > 042117_pfamA.txt.gz
+
+agfusion build --dir . --species homo_sapiens --release 75 --pfam 042117_pfamA.txt
+agfusion build --dir . --species homo_sapiens --release 76 --pfam 042117_pfamA.txt
+agfusion build --dir . --species homo_sapiens --release 77 --pfam 042117_pfamA.txt
+agfusion build --dir . --species homo_sapiens --release 78 --pfam 042117_pfamA.txt
+agfusion build --dir . --species homo_sapiens --release 79 --pfam 042117_pfamA.txt
+agfusion build --dir . --species homo_sapiens --release 80 --pfam 042117_pfamA.txt
+agfusion build --dir . --species homo_sapiens --release 81 --pfam 042117_pfamA.txt
+agfusion build --dir . --species homo_sapiens --release 82 --pfam 042117_pfamA.txt
+agfusion build --dir . --species homo_sapiens --release 83 --pfam 042117_pfamA.txt
+agfusion build --dir . --species homo_sapiens --release 84 --pfam 042117_pfamA.txt
+agfusion build --dir . --species homo_sapiens --release 85 --pfam 042117_pfamA.txt
+agfusion build --dir . --species homo_sapiens --release 86 --pfam 042117_pfamA.txt
+agfusion build --dir . --species homo_sapiens --release 87 --pfam 042117_pfamA.txt
+
+agfusion build --dir . --species mus_musculus --release 67 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 68 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 69 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 70 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 71 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 72 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 73 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 74 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 75 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 76 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 77 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 78 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 79 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 80 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 81 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 82 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 83 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 84 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 85 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 86 --pfam 042117_pfamA.txt
+agfusion build --dir . --species mus_musculus --release 87 --pfam 042117_pfamA.txt
